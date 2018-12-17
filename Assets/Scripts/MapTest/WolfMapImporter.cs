77e56839e172f75ad0a067635165d30019e5ff39
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2018-12-17 23:42:35 +0900

    Add function reading map data of binary format

--- Assets/Scripts/MapTest/WolfMapImporter.cs
+++ Assets/Scripts/MapTest/WolfMapImporter.cs
@@ -3,28 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// 0x00000000~ :00が10バイト並ぶ
-/// 0x0000000A~ :マップデータを表す識別子"WOLFM"(5byte)
-/// 0x0000000F:	未調査データ。0x64 0x65 0x82 などが含まれていることがある(19byte)
-/// 0x00000022:	マップチップID。4バイト整数型
-/// 0x00000026: マップ幅。4バイト整数型
-/// 0x0000002A: マップ高さ。4バイト整数型
-/// 0x0000002E: イベント数
-/// 0x00000032: マップ幅* 高さ*4 マップデータ
-/// 4バイト整数型でマップチップIDが並ぶ
-/// ここから始まらないこともある模様
-/// 0レイヤー目、(0,0)の位置から開始し、レイヤーごとにまとまって記述
-/// 
-/// オートタイルのidについて
-/// 3byte目がオートタイル番号、1,2byte目でオートタイルの位置を指定している？
-/// 
-/// マップデータの後ろ EOFまで イベントデータ
+/// <参考>http://kameske027.php.xdomain.jp/analysis_woditor.php
+/// マップデータは上から下に書き込まれている
 /// </summary>
 
-public class WolfMapImporter : MonoBehaviour {
+public class WolfMapImporter : MonoBehaviour
+{
+    [SerializeField]
+    TextAsset rawdata;
 
-    int[] ExportMap()
+    private void Start()
     {
+        ExportMapdata(rawdata);
+    }
 
+    Texture2D RenderMap(int[,][] mapdata)
+    {
+        return null;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="mapdata">mapdata.bytes</param>
+    /// <returns>int[mapHeight,mapWidth][layerCnt(3)]</returns>
+    int[,][] ExportMapdata(TextAsset rawdata)
+    {
+        Debug.Log(rawdata.bytes.Length);
+        byte[] bytes = rawdata.bytes;
+
+        int mapWidth = IntFromBynary(bytes, 38);
+        int mapHeight = IntFromBynary(bytes, 42);
+        long mapSize = mapWidth * mapHeight;
+        var mapdata = new int[mapHeight, mapWidth][];
+        long offset = 50;
+
+        for (int w = 0; w < mapWidth; w++)
+        {
+            for (int h = 0; h < mapHeight; h++)
+            {
+                mapdata[h, w] = new int[3];
+                for (int i = 0; i < 3; i++)
+                {
+                    long tempoffset = offset + mapSize * i;
+                    mapdata[h, w][i] = IntFromBynary(bytes, tempoffset);
+                }
+            }
+        }
+
+        Debug.Log(mapWidth+" "+mapHeight);
+        return mapdata;
+    }
+
+    int IntFromBynary(byte[] data, long offset, bool littleEndian = true)
+    {
+        if (data.Length <= offset + 4) return 0;
+
+        int val = 0;
+        for(int i = 0; i < 4; i++)
+        {
+            val += data[offset + i] << (i * 8);
+        }
+        return val;
     }
 }

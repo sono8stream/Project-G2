6d44644276b501b300eed4f9507a37b3577e7a32
sono8stream <sono.stream8580@gmail.com>
2019-02-27 13:01:38 +0900

    Add event system template

---
+++ Assets/Scripts/MapTest/EventSubject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSubject : MonoBehaviour
+{
+    EventObserver[] observers;
+
+    public void Subscribe(EventType eventType, int x, int y)
+    {
+
+    }
+}
+
+public enum EventType
+{
+    Manual, Auto, Async, PlayerTouch, EventTouch
+}
